9342d422dc16fc88de725c5944ad98cb9ce1130d
BalyshevArtem <43214667+BalyshevArtem@users.noreply.github.com>
2020-10-23 14:35:21 +0300

    OpenMP3

--- OpenMP3/src/main.cpp
+++ OpenMP3/src/main.cpp
@@ -9,9 +9,52 @@ double func(double x)
   return sin(x);
 }
 
-double calc(double x0, double x1, double dx, uint32_t num_threads)
+
+double kehan_alg( double *values, int num_steps)
 {
-  return 0;
+    double sum, c;
+    double t, y;
+    
+    sum = 0.0;
+    c = 0.0;
+
+    for ( int i = 0; i < num_steps; i++) {
+        y = values[i] - c;
+        t = sum + y;
+        c = (t - sum) - y;
+        sum = t;
+    }
+
+    return sum;
+}
+
+double calc( double x0, double x1, double dx, uint32_t num_threads)
+{
+    int num_steps;
+    double *values;
+    double sum;
+    
+    /* Считаем кол-во шагов, необходимое для подсчета интеграла */
+    num_steps = ( x1 - x0) / dx;
+    
+    /* Выделяем массив для хранения значения sin на каждом шаге */
+    values = ( double*)calloc( num_steps, sizeof( double) );
+    
+    /* Считаем параллельно значение в num_threads потоках значение синуса в различных точках*/
+    #pragma omp parallel for num_threads(num_threads)
+    for ( int i = 0; i <= num_steps; i++) {
+        values[i] =  func( x0 + i * dx);
+    }
+    
+    for ( int i = 0; i < num_steps; i++) {
+        values[i] = ( ( values[i] + values[i + 1]) * dx) / 2;
+    }
+
+    /* Алгоритм для более точного подсчета суммы */
+    sum = kehan_alg( values, num_steps);
+
+    free(values);
+    return sum;
 }
 
 int main(int argc, char** argv)
